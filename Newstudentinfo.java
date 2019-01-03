936375104ade5e652ca61d04515fbb17e015adec
java <365641531@qq.com>
2019-01-03 22:55:07 +0800

    更新增加信息

---
+++ Newstudentinfo.java
@@ -0,0 +1,45 @@
+package com.offcn.pojo;
+
+import java.math.BigDecimal;
+
+public class Newstudentinfo {
+    private Integer id;
+
+    private String name;
+
+    private BigDecimal score;
+
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
